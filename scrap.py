9015631fdc60433d8c4c3d5a1dc72a2aa5882a66
gboada23 <gustavoboadalugo@gmail.com>
2023-06-02 13:27:38 -0400

    MEJORA DEL SCRAP

--- scrap.py
+++ scrap.py
@@ -5,43 +5,32 @@ import pandas as pd
 from datetime import datetime
 
 def equipo_avg():
-    # Realizar la solicitud HTTP a la página
     url = "https://www.mlb.com/es/stats/team"
     response = requests.get(url)
-
-    # Crear el objeto BeautifulSoup para analizar el contenido HTML
     soup = BeautifulSoup(response.text, "html.parser")
 
-    # Encontrar todos los elementos <span> con la clase "full-3fV3c9pF"
-    span_elements = soup.find_all("span", class_="full-3fV3c9pF")
-    equipo = []
-    promedio = []
-    liga = []
+    teams = [span.text for span in soup.select('span.full-3fV3c9pF')]
+    leagues = [td.text for td in soup.select('td.col-group-end-2UJpJVwW.number-aY5arzrB.align-left-3L2SU-Mk.is-table-pinned-1WfPW2jT')]
+    avg_elements = soup.select('td.col-group-start-sa9unvY0.number-aY5arzrB.align-right-3nN_D3xs.is-table-pinned-1WfPW2jT')
     
-    for span in span_elements:
-        equipo.append(span.text)
-    league = soup.find_all("td", {"scope": "row", "class": "col-group-end-2UJpJVwW number-aY5arzrB align-left-3L2SU-Mk is-table-pinned-1WfPW2jT"})
+    avgs = []
+    for avg in avg_elements:
+        if avg.get('data-col') == '14':
+            avgs.append(avg.text)
 
-    for td in league:
-        liga.append(td.text)
-
-    avg = soup.find_all("td", {"scope": "row","data-col":14,"class": "col-group-start-sa9unvY0 number-aY5arzrB align-right-3nN_D3xs is-table-pinned-1WfPW2jT"})
-    # Imprimir los datos
-    for dato in avg:
-        promedio.append(dato.text)   
-    df = pd.DataFrame({"EQUIPO": equipo, "LIGA":liga, "AVERAGE":promedio})
+    df = pd.DataFrame({"EQUIPO": teams, "LIGA": leagues, "AVERAGE": avgs})
     df.AVERAGE = df.AVERAGE.astype(float)
     return df
+
 
 def records():
     fecha = datetime.today().strftime("%Y-%m-%d")
     session = HTMLSession()
     url = f'https://www.mlb.com/standings/mlb/{fecha}'
     response = session.get(url)
-    response.html.render()
+    response.html.render(timeout=10)
     teams = response.html.find('tr')
     data = []
-
     for team in teams:
         team_link = team.find('span.team--name a.team.p-text-link--mlb', first=True)
         if team_link is not None:
@@ -59,9 +48,11 @@ def records():
                 'RECORD HOMECLUB': home_record,
                 'RECORD VISITANTE': away_record})
     df_recor = pd.DataFrame(data)
-    df_recor = df_recor.head(30)
-    return df_recor
-promedio = equipo_avg()
-posiciones = records()
-print(promedio)
-print(posiciones)
+    df_recor = df_recor.drop_duplicates()
+    df_equipo_avg = equipo_avg()
+    MLB = pd.merge(df_recor,df_equipo_avg, on="EQUIPO", how="inner")
+    return MLB
+
+
+DF_TEAM_STATS = records()
+print(DF_TEAM_STATS)
